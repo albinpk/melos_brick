682b1047e74d4de468b7aff1258369b8eb47b4bd
Albin <56157868+albinpk@users.noreply.github.com>
2024-11-11 22:48:19 +0530

    feat: running `melos bootstrap` in post hook

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -3,10 +3,14 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('Installing melos...');
-
   // Adding melos as a dev dependency
+  final installingMelosProgress =
+      context.logger.progress('Installing melos...');
   await Process.run('dart', ['pub', 'add', 'melos', '--dev']);
+  installingMelosProgress.complete();
 
-  progress.complete();
+  // Running "melos bootstrap"
+  final melosBootstrapProgress = context.logger.progress('Melos bootstrap...');
+  await Process.run('melos', ['bootstrap']);
+  melosBootstrapProgress.complete();
 }
