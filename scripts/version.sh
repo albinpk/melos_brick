fe3712b21e64aaae81e8fc21ec8ca4ab31632a1e
Albin <56157868+albinpk@users.noreply.github.com>
2025-06-10 20:36:59 +0530

    chore: update version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,7 +6,10 @@ echo "Updating package version in brick.yaml"
 
 newVersion=$(grep '^version: [0-9]\+.[0-9]\+.[0-9]\+.*$' pubspec.yaml | awk '{print $2}')
 brickFile="brick.yaml"
-sed -i "/version/c\version: $newVersion" $brickFile
-git add $brickFile
+
+# Use 's' (substitute) command instead of 'c' (change)
+# This finds the line starting with 'version:' and replaces the entire line.
+sed -i '' "s/^version:.*/version: $newVersion/" "$brickFile"
+git add "$brickFile"
 
 echo "brick.yaml updated"
